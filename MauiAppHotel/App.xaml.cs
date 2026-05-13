f9a2e06bf8800dc5dd4c05c4435335efcbc8f3c7
7N.Nicolas <7n.nicolas@gmail.com>
2026-05-13 14:42:58 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppHotel/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace MauiAppHotel
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            // É necessário definir a MainPage para o app abrir algo
+            MainPage = new AppShell();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            var window = base.CreateWindow(activationState);
+
+            // Definindo dimensões iniciais para Windows/Mac
+            window.Width = 400;
+            window.Height = 600;
+
+            return window;
+        }
+    }
+}
